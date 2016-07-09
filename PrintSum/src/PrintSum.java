fb7d49d93688663461d68094ccdb6ec970d7ef53
artidotia <artidotia@gmail.com>
2016-07-09 10:19:05 +0530

    Java Session 1 Assignment 1 First Commit
    Java Session 1 Assignment 1 First Commit

---
+++ PrintSum/src/PrintSum.java
@@ -0,0 +1,10 @@
+
+public class PrintSum {
+	public static void main(String[] args) {
+		int num1 = 10, res;
+		float num2 = (float) 10.5; //or use 10.5F
+		res = (int) (num1 + num2); //convert to int
+		System.out.println("Sum of num1 and num2 is:" + res);
+	}
+
+}
